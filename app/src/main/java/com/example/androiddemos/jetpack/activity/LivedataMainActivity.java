fdd4ee13d0a5cddd90c645936a3a6b1b9c049c90
chenkexi <kexi_chen@huace.cn>
2023-07-29 14:35:13 +0800

    游戏设计说明

--- app/src/main/java/com/example/androiddemos/jetpack/activity/LivedataMainActivity.java
+++ app/src/main/java/com/example/androiddemos/jetpack/activity/LivedataMainActivity.java
@@ -16,10 +16,17 @@ import com.example.androiddemos.R;
 import com.example.androiddemos.jetpack.model.LivedataModel;
 import com.example.androiddemos.jetpack.viewmodel.LivedataViewModel;
 import com.example.androiddemos.view.RoundView;
-//做一个小游戏，有可以点击变颜色的框，和点击开始自动开始播放颜色的框，点击停止，当两个颜色一致就toast成功，反之失败。
-//change.setBackgroundResource(R.drawable.rollbutton_shape);
 //上面的圆选择后存进数据库，下面的圆在界面进行判断
-
+/**
+ * 游戏设计：1.一进来有一个弹窗需要输入名字，点击确认，页面显示当前玩游戏的人是谁，游戏开始，可以选上面的圆切换颜色，点击开始，下面的圆颜色会变，当变
+ * 的时候点击停止，若上下两个圆一样就可以继续玩，当出现无法上下一致的时候，一局结束，记录连续选中次数，弹框游戏结束，共连续多少次，下一位玩家谁：输入
+ * 名字，以此反复。有排行榜可以看自己的名字排行在多少，仅显示前10
+ * 数据库设计：每一列元素：姓名，连续次数；数据库每次插入后需要排序（按连续次数排，最多的在最前面）
+ * 颜色存储：键值对，用Mmkv存储
+ *
+ * @author chenkexi
+ * @date :2023/7/27
+ */
 public class LivedataMainActivity extends BaseActivity implements View.OnClickListener {
     private LivedataViewModel livedataViewModel;
     private final String TAG = "superdemo/JetpackMainActivity";
