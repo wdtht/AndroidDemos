5364298fb26390d39327da4f0d0098f1e327eeff
chenkexi <kexi_chen@huace.cn>
2023-08-31 19:45:48 +0800

    增加依赖

--- cardviewlibrary/src/main/java/com/example/cardviewlibrary/CardMainActivity.kt
+++ cardviewlibrary/src/main/java/com/example/cardviewlibrary/CardMainActivity.kt
@@ -1,29 +1,25 @@
 package com.example.cardviewlibrary
 
-import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.cardviewlibrary.databinding.ActivityCardMainBinding
 import com.didi.drouter.annotation.Router
 import com.didi.drouter.api.DRouter
-
 const val TAG = "superdemo/CardMainActivity"
 
 @Router(path = "/super/cardView/activity/cardView")
 class CardMainActivity : BaseViewBindingActivity<ActivityCardMainBinding>(), View.OnClickListener {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_main)
+
+    override fun getViewBinding(): ActivityCardMainBinding {
+        return ActivityCardMainBinding.inflate(layoutInflater)
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
+        Log.d(TAG, "initView !")
     }
 
-    override fun getViewBinding(): ActivityCardMainBinding {
-        TODO("Not yet implemented")
-    }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        Log.d(TAG, "onClick !")
     }
 }
